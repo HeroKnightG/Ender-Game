07f18313c7d01d0ea209caab93e3415f470093c9
HeroKnightG <asayhero69@gmail.com>
2023-12-28 19:21:14 +0700

    Special tuition calculation program Mathematics tutor
    Special tuition calculation program Mathematics tutor, one by this tutor Tuition is charged hourly, 450 baht per hour, and is paid on a one-time basis.

---
+++ Ender.cpp
@@ -0,0 +1,13 @@
+//Special tuition calculation program Mathematics tutor, one by this tutor Tuition is charged hourly, 450 baht per hour, and is paid on a one-time basis.
+#include <iostream>
+
+using namespace std ;
+
+int main() {
+    int a , b = 450; 
+    cout << "Enter Hours : " ;
+    cin >> a ;
+    int x = a * b ;
+    cout << "Total : " << x ;
+    return 0 ;
+}
